eb0e6b0d7781be9d29eac1575ab53f4c8a27c670
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-07-03 02:59:32 +0530

    Added google auth using next-auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 import Link from "next/link"
 import { Icons } from "./Icons"
 import { buttonVariants } from "./ui/Button"
+import { getAuthSession } from "@/lib/auth"
 
-const Navbar = () => {
+// asyc - server component
+const Navbar = async() => {
+  const session = await getAuthSession()
     return (
         <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
       <div className='container max-w-7xl h-full mx-auto flex items-center justify-between gap-2'>
@@ -11,7 +14,7 @@ const Navbar = () => {
           <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
           <p className='hidden text-zinc-700 text-sm font-medium md:block'>Postit</p>
         </Link>
-        <Link href='/sign-in' className={buttonVariants()}>Sign In</Link>
+        {session ? (<p>You are logged in</p>) :<Link href='/sign-in' className={buttonVariants()}>Sign In</Link>}
      
       </div>
     </div>
