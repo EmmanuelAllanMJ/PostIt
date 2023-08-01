87d3b2b16a8d4e81c94c0e1e68191efed11e79bf
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-01 22:26:57 +0530

    Deployment fix

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -8,45 +8,6 @@ const containerName = `postit`;
 const sasToken = process.env.SAS_TOKEN;
 const storageAccountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 
-export const isStorageConfigured = () => {
-  return !storageAccountName || !sasToken ? false : true
-}
-
-// console.log("URL",`https://postit1.blob.core.windows.net/${sasToken}`);
-
-const getBlobsInContainer = async (containerClient: ContainerClient) => {
-  const returnedBlobUrls: string[] = []
-
-  for await (const blob of containerClient.listBlobsFlat()) {
-    returnedBlobUrls.push(
-      `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`,
-    )
-  }
-  console.log("Returned Blob Urls", returnedBlobUrls)
-  return returnedBlobUrls
-}
-const createBlobInContainer = async (containerClient: ContainerClient, file: Blob) => {
-  const blobClient = containerClient.getBlockBlobClient(nanoid())
-  const options = { blobHTTPHeaders: { blobContentType: file.type } }
-
-  await blobClient.uploadData(file, options)
-}
-
-const uploadFileToBlob = async (file: Blob | null): Promise<string[]> => {
-  if (!file) return []
-
-  const blobService = new BlobServiceClient(process.env.AZURE_STORAGE_CONNECTION_STRING!);
-
-
-  const containerClient: ContainerClient = await blobService.getContainerClient(containerName)
-
-  await containerClient.createIfNotExists({
-    access: 'container',
-  })
-  await createBlobInContainer(containerClient, file)
-
-  return getBlobsInContainer(containerClient)
-}
 export async function POST(req: Request) {
   // get the file or blob from request and pass to uploadFIletoBlob function
   const base64String = await req.json();
