6b71e749263a0d6e78a3be12b150526e0e8c71a4
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-11 21:12:32 +0530

    Fixed vote not higlighted in detailed post

--- src/components/post-vote/PostVoteServer.tsx
+++ src/components/post-vote/PostVoteServer.tsx
@@ -1,44 +1,47 @@
+import { getAuthSession } from '@/lib/auth'
 import { Post, Vote, VoteType } from '@prisma/client'
-import { getServerSession } from 'next-auth'
 import { notFound } from 'next/navigation'
-import { FC } from 'react'
 import PostVoteClient from './PostVoteClient'
 
 interface PostVoteServerProps {
     postId: string
     initialVotesAmt?: number
     initialVote?: VoteType | null
-    getData?: () => Promise<(Post & {votes:Vote[] }) | null>
+    getData?: () => Promise<(Post & { votes: Vote[] }) | null>
 }
 
-const PostVoteServer= async ({
+const PostVoteServer = async ({
     postId,
     initialVotesAmt,
     initialVote,
     getData
-} : PostVoteServerProps) => {
-    const session = await  getServerSession()
+}: PostVoteServerProps) => {
+    const session = await getAuthSession()
 
-    let _votesAmt : number = 0
-    let _currentVote : VoteType | null | undefined = undefined
+    let _votesAmt: number = 0
+    let _currentVote: VoteType | null | undefined = undefined
 
-    if(getData){
+    if (getData) {
         const post = await getData()
-        if(!post){
+        if (!post) {
             return notFound()
         }
-        _votesAmt = await post.votes.reduce((acc, curr) => {
-            if(curr.type === 'UP') return acc + 1
-            if(curr.type === 'DOWN') return acc - 1
+        _votesAmt = post.votes.reduce((acc, curr) => {
+            if (curr.type === 'UP') return acc + 1
+            if (curr.type === 'DOWN') return acc - 1
             return acc
         }, 0)
-        _currentVote = post.votes.find(v => v.userId === session?.user?.id)?.type 
+        _currentVote = post.votes.find(
+            (vote) => (
+                 vote.userId === session?.user.id
+            )
+          )?.type
     } else {
         _votesAmt = initialVotesAmt!
         _currentVote = initialVote
     }
 
-  return <PostVoteClient postId={postId} initialVotesAmt={_votesAmt} initialVote={_currentVote} />
+    return <PostVoteClient postId={postId} initialVotesAmt={_votesAmt} initialVote={_currentVote} />
 }
 
 export default PostVoteServer
