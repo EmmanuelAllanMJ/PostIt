e68e7506097b58325cf5fc470a53611f9fe71e75
EmmanuelAllanMJ <emmanuelallanmj@gmail.com>
2023-08-11 16:17:41 +0530

    Added detailed post view with redis support

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,20 +1,36 @@
 import * as redis from 'redis';
 
-
 // Environment variables for cache
 const cacheHostName = process.env.AZURE_CACHE_FOR_REDIS_HOST_NAME;
 const cachePassword = process.env.AZURE_CACHE_FOR_REDIS_ACCESS_KEY;
 
-if (!cacheHostName) throw Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
-if (!cachePassword) throw Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
+if (!cacheHostName) throw new Error("AZURE_CACHE_FOR_REDIS_HOST_NAME is empty");
+if (!cachePassword) throw new Error("AZURE_CACHE_FOR_REDIS_ACCESS_KEY is empty");
 
-// Connection configuration
-export const Redis = redis.createClient({
-    // rediss for TLS
-    url: `rediss://${cacheHostName}`,
-    password: cachePassword
-});
+    // export const Redis = redis.createClient({
+    //   url: `rediss://${cacheHostName}`,
+    //   password: cachePassword
+    // });
+    export const Redis = redis.createClient({ socket: { host: cacheHostName, port: 6380, tls: true }, password: cachePassword,    legacyMode: true
+    });
 
+// Create Redis cache connection
+// async function Redis() {
+//     const cacheConnection = redis.createClient({
+//       url: `rediss://${cacheHostName}`,
+//       password: cachePassword
+//     });
+  
+//     await new Promise((resolve, reject) => {
+//       cacheConnection.on('connect', resolve);
+//       cacheConnection.on('error', reject);
+//     });
+  
+//     return cacheConnection;
+//   }
+  
+// // Export the cache connection
+// export default Redis;
 
 
 // Connect to Redis
